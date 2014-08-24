1ad71f47c45cb2931bb594895cf9e3568628353e
jake <jake3@163.com>
2014-08-24 23:28:37 +0800

    persistence-append file

--- molyserver/Appender.cpp
+++ molyserver/Appender.cpp
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include "Appender.h"
-
+#include "JK_Utility.h"
+#include "LogManager.h"
 
 Appender::Appender()
 {
@@ -12,8 +14,34 @@ Appender::~Appender()
 
 }
 
+
+int Appender::LoadAppendFile()
+{
+	char tmpfile[256];
+	JK_SPRITF_S(tmpfile, 256, "molydb_af-%d.dmf", (int)JK_GETPID() );
+	FILE *fp = NULL;
+	JK_OPENFILE_S( fp,tmpfile,"w");
+	if (!fp) 
+	{
+		MOLYLOG(MOLY_LOG_ERORR, "Failed opening dump file, errno: %d!", errno);
+		return 1;
+	}
+	return 0;
+}
+
+
+
 int Appender::AppendToFile()
 {
+	char tmpfile[256];
+	JK_SPRITF_S(tmpfile, 256, "molydb_af-%d.dmf", (int)JK_GETPID() );
+	FILE *fp = NULL;
+	JK_OPENFILE_S( fp,tmpfile,"w");
+	if (!fp) 
+	{
+		MOLYLOG(MOLY_LOG_ERORR, "Failed opening dump file, errno: %d!", errno);
+		return 1;
+	}
 	return 0;
 }
 
--- molyserver/Appender.h
+++ molyserver/Appender.h
@@ -8,6 +8,7 @@ public:
 	Appender();
 	~Appender();
 
+	int LoadAppendFile();
 	int AppendToFile();
 };
 
